5f8c4830aecc7c3b78341938204dfb1d46cdfca4
serkanaplan <serkanaplan@hotmail.com>
2024-04-12 20:50:10 +0300

    ch_1_Single Responsibility

---
+++ SingleResponsibility/NotSR.cs
@@ -0,0 +1,68 @@
+﻿namespace SOLID.NotSingleResponsibility;
+
+public class Product
+{
+      
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        private static List<Product> ProductList = new List<Product>();
+
+        public Product()
+        {
+            ProductList = new()
+            {
+                new(){ Id=1, Name="Kalem 1"},
+                  new(){ Id=2, Name="Kalem 2"},
+                    new(){ Id=3, Name="Kalem 3"},
+                      new(){ Id=4, Name="Kalem 4"},
+                        new(){ Id=5, Name="Kalem 5"}
+            };
+        }
+
+
+        public void SaveOrUpdate(Product product)
+        {
+
+            var hasProduct= ProductList.Any(p => p.Id == product.Id);
+
+            if(!hasProduct)
+            {
+                ProductList.Add(product);
+            }
+            else
+            {
+                var index = ProductList.FindIndex(x => x.Id == product.Id);
+
+                ProductList[index] = product;
+
+            }
+
+        }
+
+        public void Delete(int id)
+        {
+            var hasProduct = ProductList.Find(x => x.Id == id);
+
+            if(hasProduct==null)
+            {
+
+                throw new Exception("Ürün bulunamadı");
+            }
+
+            ProductList.Remove(hasProduct);
+           
+        }
+
+
+        public void WriteToConsole()
+        {
+
+            ProductList.ForEach(x =>
+            {
+                Console.WriteLine($"{x.Id} - {x.Name} ");
+            });
+        }
+
+
+}
